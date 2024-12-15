f7c9ad6aee8c78563a232f126d1502b30dc65f0d
git-code-michael <sudomichaelicloud@gmail.com>
2024-12-14 20:30:58 -0500

    feat: add Asteroid class and integrate with AsteroidField for spawning system

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 import constants
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -17,10 +19,15 @@ def main():
 
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     
     Player.containers = (updateable, drawable)
-    
+    Asteroid.containers = (updateable, drawable, asteroids)
+    AsteroidField.containers = (updateable)
+
     player = Player(x = constants.SCREEN_WIDTH / 2, y = constants.SCREEN_HEIGHT / 2)
+
+    AsteroidField()
 
     while True:
         for event in pygame.event.get():
